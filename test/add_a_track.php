93f7accbfcdbcc356ddd5721f6db4c7721b5c041
AbarcaS <s.abarca714@gmail.com>
2016-12-28 03:11:59 -0800

    Add a track

--- test/add_a_track.php
+++ test/add_a_track.php
@@ -1,4 +1,4 @@
-<h1>Artist Editor </h1>
+<h1>Song Editor </h1>
 <?php
 
 
@@ -14,7 +14,6 @@ var_dump( $_POST);
 
 
 
-
 if(isset($_POST['action']))
 {
 
@@ -22,37 +21,42 @@ if(isset($_POST['action']))
 	if($_POST['action'] == 'create' ){
 
 	$name = $_POST['name'];
-	$origin = $_POST['origin'];
-	$gender = $_POST['gender'];
+	$artist = $_POST['artist'];
+	$album = $_POST['album'];
+	$genre = $_POST['genre'];
+	$year = $_POST['year'];
 
 	if($_POST['id'] != ''){
 		echo "i have an id";
-		$sql = "update `artist` set `name` = :name, `origin` = :origin, `gender` = :gender WHERE `id`=:id";
+		$sql = "UPDATE `songs` set `name` = :name, `artist` = :artist, `album` = :album, `genre` = :genre, `year`= :year WHERE `id`=:id";
 		echo $sql . "<br>";
 		$stm = $dbh->prepare($sql);
 		$arr = ['name' => $name,
-				'origin' => $origin,
-				'gender' => $gender,
+				'artist' => $artist,
+				'album' => $album,
+				'genre' => $genre,
+				'year'=> $year,
 				'id' => $_POST['id']
 		];
 		var_dump($arr);
 $status = $stm->execute($arr);
-
-
+var_dump($stm->errorInfo());
 	}else{
 		echo "i dont have an id";
-		$sql = "INSERT INTO `playlist_site`.`artist` (`name`, `origin`, `gender`) VALUES (:name, :origin, :gender)";
+		$sql = "INSERT INTO `playlist_site`.`songs` (`name`, `artist`, `album`, `genre`, `year`) VALUES (:name, :artist, :album, :genre, :year)";
 		echo $sql . "<br>";
 		$stm = $dbh->prepare($sql);
 		$arr = ['name' => $name,
-		'origin' => $origin,
-		'gender' => $gender];
+				'artist' => $artist,
+				'album' => $album,
+				'genre' => $genre,
+				'year' => $year];
 		$status = $stm->execute($arr); //Returns TRUE on success or FALSE on failure.
 
 
 		if($status == false){
 
-			echo "Artist : {$name}s Already exists";
+			echo "Song : {$name} Already exists learn to type";
 		}
 
 		}
@@ -64,19 +68,21 @@ $status = $stm->execute($arr);
 
 ?>
 
-<form action="form_test.php" method="POST" accept-charset="utf-8">
+<form action="add_a_track.php" method="POST" accept-charset="utf-8">
 	
 	name<input type="text" name="name" value="" placeholder=""><br>
-	origin<input type="text" name="origin" value="" placeholder=""><br>
-	gender<input type="text" name="gender" value="" placeholder=""><br>
-	<input type="hidden" name="id" value="<?php echo $id; ?>" placeholder="">
+	artist<input type="text" name="artist" value="" placeholder=""><br>
+	album<input type="text" name="album" value="" placeholder=""><br>
+	genre<input type="text" name="genre" value="" placeholder=""><br>
+	year<input type="text" name="year" value="" placeholder=""><br>
+	<input type="text" name="id" value="<?php echo $id; ?>" placeholder="">
 	<input type="submit" name="action" value="create">
 </form>
 
 
 <?php
 //display
- $stm = $dbh->query(" select * FROM `artist`");
+ $stm = $dbh->query(" select id, name, artist, album, genre, year FROM `songs`");
 echo "<table border='1'>";
  while($row = $stm->fetch(PDO::FETCH_ASSOC)){
 
